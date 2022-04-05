f8310919d0fdcdfd158037b55edee09f45cc6205
Fox, Lowella <lowella.fox@verizon.com>
2022-04-05 10:39:53 -0500

    Adding files

---
+++ app.js
@@ -0,0 +1,127 @@
+const express = require("express");
+//require basicAuth
+const basicAuth = require('express-basic-auth');
+//require bcrypt
+const bcrypt = require('bcrypt');
+// set salt
+const saltRounds = 2;
+
+const {User, Item} = require('./models');
+const { use } = require("bcrypt/promises");
+
+// initialise Express
+const app = express();
+
+// specify out request bodies are json
+app.use(express.json());
+
+// configure basicAuth
+app.use(basicAuth({
+  authorizer : dbAuthorizer,
+  authorizeAsync : true,
+  unauthorizedResponse : () => "You do not have access to this content"
+}))
+
+// compare username and password with db content
+// return boolean indicating password match
+async function dbAuthorizer(username, password, callbak){
+  try {
+    // get matching user from db
+    const user = await User.findOne({where: {name:username}})
+    // if username is valid compare passwords
+    let isValid = (user != null ) ? await bcrypt.compare(password, user.password) : false;
+    callbak(null, isValid)
+  } catch(err) {
+    //if authorize fails, log error
+    console.log("Error: ", err)
+    callbak(null, false)
+  }
+}
+
+// routes go here
+app.get('/', (req, res) => {
+  res.send('<h1>App Running</h1>')
+})
+
+// read all users
+app.get('/users', async (req,res) =>{
+  let users = await User.findAll()
+  res.json({users})
+})
+
+// read one user by id
+app.get('/users/:id', async (req, res) => {
+  let user = await User.findByPk(req.params.id);
+  res.json({user});
+})
+
+// create user
+app.post('/users', async (req,res) => {
+  const name = req.body.name;
+  const password = req.body.password;
+
+  bcrypt.hash(password, saltRounds, async function (err,hash){
+    const newUser = await User.create({'name': name, 'password': hash})
+    res.json({newUser})
+  })
+})
+
+//create new session
+app.post('/session', async (req,res) => {
+  const thisUser = await User.findOne({
+    where: {name: req.body.name}
+  })
+  if(!thisUser){
+    res.send("User not found")
+  } else {
+    bcrypt.compare(req.body.password, thisUser.password, async function (err,result) {
+      if(result){
+        console.log(thisUser.password)
+        res.json(thisUser)
+      } else {
+        res.send("Passwords do not match")
+      }
+    })
+  }
+
+})
+
+
+
+// read all items
+app.get('/items', async(req, res)=> {
+  let items = await Item.findAll();
+  res.json({items});
+})
+
+// read item by id
+app.get('/items/:id', async(req, res)=> {
+  let item = await Item.findByPk(req.params.id);
+  res.json({item});
+})
+
+// delete item
+app.delete('/items/:id', async(req, res)=> {
+  await Item.destroy({where: {id: req.params.id}});
+  res.send('Deleted!')
+})
+
+// create item
+app.post('/items', async(req, res)=> {
+  let newItem = await Item.create(req.body);
+  res.json({newItem})
+})
+
+// update item
+app.put('/items/:id', async(req, res)=> {
+  let updatedItem = await Item.update(req.body, {
+    where : {id : req.params.id}
+  });
+  res.json({updatedItem})
+})
+
+
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
---
+++ seed.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const fs = require('fs').promises;
+const bcrypt = require('bcrypt');
+
+const {sequelize} = require('./db');
+const {User, Item} = require('./models');
+
+const createUsers = async () => {
+
+    let pw1 = await bcrypt.hash('1234', 2);
+    let pw2 = await bcrypt.hash('password', 2);
+
+    const users = [
+        {name : 'Dan', password: pw1 },
+        {name : 'Linda', password : pw2}
+    ];
+
+    return users
+}
+
+
+const items = [
+    {name : 'Gold'},
+    {name : 'Silver'},
+    {name : 'Paladium'}
+];
+
+
+const seed = async () => {
+
+    await sequelize.sync({ force: true });
+
+    const users = await createUsers(); // create users w/ encrypted passwords
+
+    const userPromises = users.map(user => User.create(user))
+    const itemPromises = items.map(item => Item.create(item))
+    await Promise.all([...userPromises, ...itemPromises]);
+    console.log("db populated!")
+}
+
+seed();
